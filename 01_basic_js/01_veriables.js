388b37a3e25473c35af11dc6911e76203a6f48fa
Gangulii123456789 <gangugangadhar99@gmail.com>
2025-11-28 00:16:10 +0530

    Add files via upload

---
+++ 01_basic_js/01_veriables.js
@@ -0,0 +1,19 @@
+// variables = let , var 
+// constants = const 
+
+const Accountid =123456;
+
+let accountname ="gangu";  
+
+var accountbranch ="terdal";
+
+accountcity ="karnataka";
+
+ let account;
+
+
+console.log([Accountid,accountname,accountbranch]);
+
+
+
+
