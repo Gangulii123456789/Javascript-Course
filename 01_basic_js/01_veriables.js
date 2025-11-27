f3232e8bd97173f69c8778210a9476ad1c9d6f1d
Gangulii123456789 <gangugangadhar99@gmail.com>
2025-11-28 00:21:33 +0530

    Refactor variable declarations and comments
    Updated comments and clarified variable declarations.

--- 01_basic_js/01_veriables.js
+++ 01_basic_js/01_veriables.js
@@ -1,18 +1,27 @@
 // variables = let , var 
 // constants = const 
 
+
+// const dose not change the value 
 const Accountid =123456;
-
+//let can be modify
 let accountname ="gangu";  
-
+//var is older version and it can also modify the value
 var accountbranch ="terdal";
 
 accountcity ="karnataka";
-
- let account;
-
-
+let account;
 console.log([Accountid,accountname,accountbranch]);
+
+
+
+//HOISTING MEANS CALLING OR ACCESING A VERIABLE WITHOUT DECLEARING OR ASSIGNING A VALUE FOR IT.
+
+
+//var supports hoisting 
+//let does not support hoisting 
+//const also does not support hoisting
+
 
 
 
